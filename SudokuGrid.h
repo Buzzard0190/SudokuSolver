7aa0b9acf9b1b2b93cca275efd78965a200efc04
Buzzard0190 <cbhbmx@gmail.com>
2016-02-09 15:40:36 -0800

    updated to not print out the input twice

--- SudokuGrid.h
+++ SudokuGrid.h
@@ -26,9 +26,7 @@ public:
     
     //Constructor that creates initial puzzle from an input string
     SudokuGrid(std::string s) {
-        
-        std::cout << s <<"\n";
-        
+                
         for (int i = 8; i >= 0; i--) {
             for (int j = 8; j >= 0; j--) {
                 int r = s.back()-48;
